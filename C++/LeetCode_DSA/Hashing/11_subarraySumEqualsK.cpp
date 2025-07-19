364d4e70681394f298149f7c9015f50c7efab80b
Arun <arumad.10@gmail.com>
2025-07-19 18:55:17 +0200

    Create 11_subarraySumEqualsK.cpp

---
+++ C++/LeetCode_DSA/Hashing/11_subarraySumEqualsK.cpp
@@ -0,0 +1,30 @@
+/* Example 4: 560. Subarray Sum Equals K
+Given an integer array nums and an integer k, find the number of subarrays whose sum is equal to k.
+*/
+
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+
+using namespace std;
+
+int subarraySum(vector<int> ele, int k) {
+    unordered_map<int,int> temp;
+    temp[0] = 1;                //  subarray initial value is 0:1 , it means the sum - k = 0 that instance we need to store by default
+    int ans = 0, curr = 0;
+    for(int num : ele){
+        curr += num;
+        ans += temp[curr -k];       //  here now it is temp[1-3]=>temp[-2] so there is no -2 indices so this wont do anything only the ans is updated with ans = ans + 0;
+        temp[curr]++;
+    }
+    cout<<endl;
+    return ans;
+}
+
+int main(){
+    vector<int> val{1,2,1,2,1};
+    int k = 3;
+    cout<<"Subarray Sum : "<<subarraySum(val,k)<<endl;
+    return 0;
+
+}

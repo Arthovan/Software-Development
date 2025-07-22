256ddebd220cfcebd2216b2efe3a3199d0d0959f
Arthovan <arumad.10@gmail.com>
2025-07-22 17:54:48 +0200

    feat: Updated of double prime

--- C++/Interview/cisco_interview/doublePrime.cpp
+++ C++/Interview/cisco_interview/doublePrime.cpp
@@ -1,6 +1,4 @@
-// Find the prime numbers from 1 to K and insert in the 2D array as double prime values
-// Ex: prime from 1 to 10 is 2,3,5,7
-// O/P : [3,5],[5,7]  == > double prime means differnce between adjacent prime is 2 
+// Find prime numbers up to a given number K and then identify pairs of "double primes"(pair of primes that differ by 2).
 
 #include<iostream>
 #include<string>
@@ -12,7 +10,7 @@ vector<vector<int>> prime(int data){
     vector<int> temp;
     vector<vector<int>> res;
     int cnt = 0;
-    //temp.push_back(2);
+    // find all prime numbers upto data
     for(int i = 2; i<=data;i++)
     {   
         cnt = 0;
@@ -24,33 +22,22 @@ vector<vector<int>> prime(int data){
             temp.push_back(i);
         }
     }
-
-    for(auto ele : temp){
-        cout<<ele<< " ";
-    }
-
-    int iterate = 0;
+    // find double primes
     for(int i = 0; i < (temp.size())-1;i++){
-        int diff = temp[i+1] - temp[i];
-        if(diff == 2)
+        if((temp[i+1] - temp[i]) == 2)
         {
-            res[iterate][0] = temp[i];
-            res[iterate][1] = temp[i+1];
-            i++;
-            iterate++;
+            res.push_back({temp[i],temp[i+1]});
         }
     } 
-
     return res;
 }
 
 int main(){
-   int val = 50; // 101
+   int val = 50; 
    vector<vector<int>> temp = prime(val);
-   int len = temp.size()-1;
    for(vector<int> val: temp)
    {
-    cout<<val[0]<<" "<<val[1];
+    cout<<"["<<val[0]<<", "<<val[1]<<"] ";
    }
    cout<<endl;
    return 0;

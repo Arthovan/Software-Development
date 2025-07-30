0e9c2cb313cc902a5fdd1f8f217d34a0bd33fdc4
Arun <arumad.10@gmail.com>
2025-07-30 22:11:26 +0200

    Create 29_pthread_Producer_Consumer_3.c

---
+++ C/Unix_Threads_C/29_pthread_Producer_Consumer_3.c
@@ -0,0 +1,170 @@
+/* Things to consider in the producer and cosumer problem
+    1. Manage shared memory access ==> Bcs thread access the shared memory so we need to make sure no deadlock occurs there
+    2. Checking for if buffer is full ==> If we have 2 producer and 1 consumer then the buffer will be filled soon, so we need to make sure buffer is full or not
+    3. Checking for if buffer is empty ==> If we have 1 producer and 2 consumer then the buffer will be accessed even if it is empty so we need to make sure its not empty
+*/
+
+// This below program is a basic one of producer, consumer problem. Will see the mutex and semaphore based in the upcoming codes
+// Generally producer like web and consumer like server always producing & consuming something, so we need to make sure its running in loop
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<time.h>
+#include<semaphore.h>
+
+#define THREAD_NUM 8
+#define PROPER_CODE
+
+int buffer[10];
+int count = 0;
+
+pthread_mutex_t mutexBuffer;
+
+sem_t semFull;
+sem_t semEmpty;
+
+#ifdef PROPER_CODE
+void* producer(void* args){
+    while(1){ // without while loop program works fine
+        // Produce
+        int x = rand() % 100;
+        sleep(1);
+
+        // Add to the buffer
+        sem_wait(&semEmpty); // so that semaphore start decrementing from the 10
+        pthread_mutex_lock(&mutexBuffer);   // Mutex added to avoid deadlock 
+        // as we can see here that the if statement is removed from here bcs the semaphore sem_wait will decrement from 10. once it reaches 0 then it will wait until the value incremented again by sem_post
+        // so semaphore itseld taking care of buffer full and empty issues so no need of the conditional statements here
+        buffer[count] = x;
+        count++;
+        pthread_mutex_unlock(&mutexBuffer);
+        sem_post(&semFull);// so that semaphore start incrementing from 0
+    }
+    //return NULL;
+}
+
+void* consumer(void* args){
+    while(1){ // without while loop program works fine
+        //int y = -1; this is alo not needed now bcs of semaphore
+        int y;
+        // Remove from the buffer
+        sem_wait(&semFull);
+        pthread_mutex_lock(&mutexBuffer);
+        // as we can see here that the if statement is removed from here bcs the semaphore sem_wait will semfull. Which was incremented in the producer routine
+        y = buffer[count - 1]; // accessing the last element in the buffer
+        count--;
+        pthread_mutex_unlock(&mutexBuffer);
+        sem_post(&semEmpty);
+
+        // Consume
+        printf("Got %d\n", y);
+        //sleep(1);
+        
+    }
+    //return NULL;
+}
+
+
+int main(int argc, char* argv[]){
+    srand(time(NULL));
+    pthread_t thread[THREAD_NUM];
+    pthread_mutex_init(&mutexBuffer, NULL);
+    sem_init(&semEmpty, 0, 10); // bcs of we have 10 elements in buffer
+    sem_init(&semFull, 0, 0);   // bcs semFull should be so that we can stack 10 elements
+    int i;
+    for(i = 0; i < THREAD_NUM; i++){
+        if(i % 2 == 0){
+            if(pthread_create(&thread[i],NULL,&producer,NULL) != 0){
+                perror("Failed to create a Thread\n");
+            }
+        } else {
+            if(pthread_create(&thread[i],NULL,&consumer,NULL) != 0){
+                perror("Failed to join a Thread\n");
+            }
+        }
+    }
+    for(i = 0; i < THREAD_NUM; i++){
+        if(pthread_join(thread[i],NULL) != 0){
+            perror("Failed to create a Thread\n");
+        }
+    }
+    pthread_mutex_destroy(&mutexBuffer);
+    sem_destroy(&semEmpty);
+    sem_destroy(&semFull);
+    return 0;
+}
+#endif
+
+#ifdef IMPROPER_CODE
+void* producer(void* args){
+    while(1){ // without while loop program works fine
+        // Produce
+        int x = rand() % 100;
+
+        // Add to the buffer
+        sem_wait(&semEmpty); // so that semaphore start decrementing from the 10
+        pthread_mutex_lock(&mutexBuffer);   // Mutex added to avoid deadlock 
+        if(count < 10){ // this is added to make sure to add elements only when the buffer is not full, this is not an optimal solution yet
+            buffer[count] = x;
+            count++;
+        }else { // we can clearly see that most of the numbers were skipped bcs of the above condition and we can see the best solution in the upcoming codes
+            printf("Skipped %d\n",x);
+        }
+        pthread_mutex_unlock(&mutexBuffer);
+        sem_post(&semFull);// so that semaphore start incrementing from 0
+    }
+    //return NULL;
+}
+
+void* consumer(void* args){
+    while(1){ // without while loop program works fine
+        int y = -1;
+        // Remove from the buffer
+        sem_wait(&semFull);
+        pthread_mutex_lock(&mutexBuffer);
+        if(count > 0){  // this is added to make sure to remove only elements only the buffer is not empty
+            y = buffer[count - 1]; // accessing the last element in the buffer
+            count--;
+        }
+        pthread_mutex_unlock(&mutexBuffer);
+        sem_post(&semEmpty);
+
+        // Consume
+        printf("Got %d\n", y);
+        
+    }
+    //return NULL;
+}
+
+
+int main(int argc, char* argv[]){
+    srand(time(NULL));
+    pthread_t thread[THREAD_NUM];
+    pthread_mutex_init(&mutexBuffer, NULL);
+    sem_init(&semEmpty, 0, 10); // bcs of we have 10 elements in buffer
+    sem_init(&semFull, 0, 0);   // bcs semFull should be so that we can stack 10 elements
+    int i;
+    for(i = 0; i < THREAD_NUM; i++){
+        if(i % 2 == 0){
+            if(pthread_create(&thread[i],NULL,&producer,NULL) != 0){
+                perror("Failed to create a Thread\n");
+            }
+        } else {
+            if(pthread_create(&thread[i],NULL,&consumer,NULL) != 0){
+                perror("Failed to join a Thread\n");
+            }
+        }
+    }
+    for(i = 0; i < THREAD_NUM; i++){
+        if(pthread_join(thread[i],NULL) != 0){
+            perror("Failed to create a Thread\n");
+        }
+    }
+    pthread_mutex_destroy(&mutexBuffer);
+    sem_destroy(&semEmpty);
+    sem_destroy(&semFull);
+    return 0;
+}
+#endif

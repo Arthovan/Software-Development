ac3c541cae0fab5f3ae9723a977e4e5092782b9f
Arun <arumad.10@gmail.com>
2025-12-02 16:34:04 +0100

    Update majorityElement-I.cpp

--- C++/TUF/Arrays/majorityElement-I.cpp
+++ C++/TUF/Arrays/majorityElement-I.cpp
@@ -1,1 +1,30 @@
-pass
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        int size = nums.size();
+        std::unordered_map<int,int> count;
+        for(int num : nums) {
+          count[num]++;
+        }
+
+        for(auto pair : count) {
+            if(pair.second > size/2) {
+                return pair.first;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<int> nums = {-2,-2,1,1,1,-2,-2};
+    Solution sol;
+    int output = sol.majorityElement(nums);
+    std::cout<<"Majority element is : "<<output<<std::endl;
+    return 0;
+}
